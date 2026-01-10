a4b9d15b2a8235ecb87711c32398ae71459241db
CH.G <35129014+parkjio3@users.noreply.github.com>
2026-01-10 20:40:13 +0900

    Update page.tsx

--- app/street/page.tsx
+++ app/street/page.tsx
@@ -99,6 +99,7 @@ const streetTricks: Trick[] = [
   { id: "sh8", category: "샤빗", name: "페이키FS샤빗", videoUrl: "" },
   { id: "sh9", category: "샤빗", name: "페이키FS빅스핀", videoUrl: "" },
   { id: "sh10", category: "샤빗", name: "FS빅스핀", videoUrl: "" },
+  { id: "sh10", category: "샤빗", name: "임파서블", videoUrl: "" },
   { id: "r1", category: "회전", name: "BS180알리", videoUrl: "" },
   { id: "r2", category: "회전", name: "BS360알리", videoUrl: "" },
   { id: "r3", category: "회전", name: "BS하프캡", videoUrl: "" },
